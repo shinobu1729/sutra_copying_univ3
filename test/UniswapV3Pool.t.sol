e91a21f4d0ac943d98f1392df62f4d4b282ee7d3
shinobu1729 <sino1729defi@gmail.com>
2023-02-05 14:52:43 +0700

    mint test (in prog)

--- test/UniswapV3Pool.t.sol
+++ test/UniswapV3Pool.t.sol
@@ -3,11 +3,59 @@
 pragma solidity ^0.8.14;
 
 import "forge-std/Test.sol";
+import "./ERC20Mintable.sol";
+import "../src/UniswapV3Pool.sol";
 
 contract UniswapV3PoolTest is Test {
-    function setUp() public {}
+    ERC20Mintable token0;
+    ERC20Mintable token1;
+    UniswapV3Pool pool;
+
+    function setUp() public {
+        token0 = new ERC20Mintable("Ether", "ETH", 18);
+        token1 = new ERC20Mintable("USDC", "USDC", 18);
+    }
 
     function testExample() public {
         assertTrue(true);
+    }
+
+    function testMintSuccess() public {
+        TestCaseParams memory params = TestCaseParams({
+            wethBalance: 1 ether,
+            usdcBalance: 5000 ether,
+            currentTick: 85176,
+            lowerTick: 84222,
+            upperTick: 86129,
+            liquidity: 1517882343751509868544,
+            currentSqrtP: 5602277097478614198912276234240,
+            shouldTransferInCallback: true,
+            mintLiqudity: true
+        });
+    }
+
+    function setupTestCase(
+        TestCaseParams memory params
+    ) internal returns (uint256 poolBalance0, uint256 poolBalance1) {
+        token0.mint(address(this), params.wethBalance);
+        token1.mint(address(this), params.usdcBalance);
+
+        pool = new UniswapV3Pool(
+            address(token0),
+            address(token1),
+            params.currentSqrtP,
+            params.currentTick
+        );
+
+        if (params.mintLiqudity) {
+            (poolBalance0, poolBalance1) = pool.mint(
+                address(this),
+                params.lowerTick,
+                params.upperTick,
+                params.liquidity
+            );
+        }
+
+        shouldTransferInCallback = params.shouldTransferInCallback;
     }
 }
